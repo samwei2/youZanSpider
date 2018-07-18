730a4a6d383cd3ee96d79dc794661bba13511318
laowai411 <laowai__2006@163.com>
2018-07-18 11:33:15 +0800

    添加创建表接口

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 from allGoodsList import GoodListController
 from aliasSellInfo import SellInfoController
 from mySqlController import SqlOperationController
@@ -7,47 +7,50 @@ import json
 
 # 全局头
 headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 QIHU 360SE"
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 QIHU 360SE"
 }
 
-#请求头
+# 请求头
 f = open('foo.txt')
-cookies={}
-for line in f.read().split(';'): 
-	name,value=line.strip().split('=',1)
-	cookies[name]=value
+cookies = {}
+for line in f.read().split(';'):
+    name, value = line.strip().split('=', 1)
+    cookies[name] = value
 
 # print cookies
 
-#商品控制
+# 商品控制
 goodListController = GoodListController(headers)
 
 sellInfoController = SellInfoController(headers, cookies)
 
 sqlOperationController = SqlOperationController()
 
-#商品道具列表
-aliasIds = goodListController.openGoodsUrl("https://h5.youzan.com/v2/allgoods/16975645?reft=1531274513986&spm=f70430959") #拿到当前页所有的商品信息列表
+# 商品道具列表
+aliasIds = goodListController.openGoodsUrl(
+    "https://h5.youzan.com/v2/allgoods/16975645?reft=1531274513986&spm=f70430959")  # 拿到当前页所有的商品信息列表
 
-#保存商品销售数据
-aliasTitleDic = {} # 商品名称字典
+# 保存商品销售数据
+aliasTitleDic = {}  # 商品名称字典
 sellInfoDic = {}  # 订单信息字典
 sellTotalDic = {}  # 产品销量字典
 # index = 0
 # oneAlias = aliasIds[3]
 for oneAlias in aliasIds:
-	id = oneAlias[u'alias']
-	title = oneAlias[u'title']
-	pageArr, total= sellInfoController.checkAliasInfo(id, title)  #单个商品 购买的详细信息
-	aliasTitleDic[id] = title
-	sellInfoDic[id] = pageArr
-	sellTotalDic[id] = total
+    id = oneAlias[u'alias']
+    title = oneAlias[u'title']
+    pageArr, total = sellInfoController.checkAliasInfo(id, title)  # 单个商品 购买的详细信息
+    aliasTitleDic[id] = title
+    sellInfoDic[id] = pageArr
+    sellTotalDic[id] = total
 pass
 
 print(" goods parser completed, ready connectSql....................")
 
 # 链接数据库
 sqlOperationController.connectSql()
+
+sqlOperationController.createTable()
 
 sqlOperationController.saveAliasInfo(aliasTitleDic, sellInfoDic, sellTotalDic)
 
--- mySqlController.py
+++ mySqlController.py
@@ -1,134 +1,157 @@
 # -*- coding: UTF-8 -*-
 import pymysql.cursors
 
+
 class SqlOperationController():
-	"""docstring for sqlOperation"""
-	def __init__(self):
-		pass
+    """docstring for sqlOperation"""
 
-	def connectSql(self):
-		# 连接数据库
-		self.__connect = pymysql.Connect(
-		    host='localhost',
-		    port=3306,
-		    user='root',
-		    passwd='',
-		    db='alias',
-		    charset='utf8'
-		)		
-		pass
-		
-	def saveAliasInfo(self, aliasTitleDic, sellInfoDic, sellTotalDic):
-		# 获取游标
-		self.__cursor = self.__connect.cursor()
-		index = 0
+    def __init__(self):
+        pass
+
+    def connectSql(self):
+        # 连接数据库
+        self.__connect = pymysql.Connect(
+            host='localhost',
+            port=3306,
+            user='root',
+            passwd='',
+            db='alias',
+            charset='utf8'
+        )
+        pass
+
+    # 创建表
+    def createTable(self):
+        self.__cursor = self.__connect.cursor()
+        sql_create = '''
+            CREATE TABLE `aliasinfo` (
+              `id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+              `buyer` varchar(255) DEFAULT NULL,
+              `time` varchar(255) DEFAULT NULL,
+              `buyCount` int(255) DEFAULT NULL,
+              `aliasId` varchar(255) DEFAULT NULL,
+              `price` decimal(8,2) DEFAULT NULL,
+              `title` varchar(255) DEFAULT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=963 DEFAULT CHARSET=utf8;
+        '''
+        self.__cursor.execute(sql_create)
+        self.__connect.commit()
+    pass
 
 
-		for key in sellInfoDic:
-			# print(sellInfoDic[key])
-			# # 一件商品的数据
-			sellInfoList = sellInfoDic[key]
-			if sellInfoList is None or len(sellInfoList)==0:
-				continue
-			
-			for sellInfo in sellInfoList:
-				infoData = sellInfo['data']
-				dealUid  = sellInfo['uid']
-				title    = sellInfo['title'] #.decode('unicode_escape')
-				data = (infoData[u'nickname'], infoData[u'update_time'], infoData[u'item_num'], dealUid, title, infoData[u'item_price'])
-				# print("ready write data", data)
-				# print(dealUid, "is ready write to sql")	
-				
-				bHasData = False
-				try:
-					sql = "SELECT aliasId FROM aliasInfo WHERE aliasId = '%s'"
-					uidData = (dealUid)
-					self.__cursor.execute(sql % uidData)
-					bHasData = len(self.__cursor.fetchall()) > 0
-					pass
-				except Exception as e:
-					print("SELECT error",e.message)
-					pass
+def saveAliasInfo(self, aliasTitleDic, sellInfoDic, sellTotalDic):
+    # 获取游标
+    self.__cursor = self.__connect.cursor()
+    index = 0
 
-				if bHasData is False:
-					try:
-						# 插入数据
-						sql = "INSERT INTO aliasInfo (buyer, time, buyCount, aliasId, title, price) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')"
-						self.__cursor.execute(sql % data)
-						self.__connect.commit()
+    for key in sellInfoDic:
+        # print(sellInfoDic[key])
+        # # 一件商品的数据
+        sellInfoList = sellInfoDic[key]
+        if sellInfoList is None or len(sellInfoList) == 0:
+            continue
 
-						index = index+1
-						if index>8:
-							index = 0
-							pass
-						str = ""
+        for sellInfo in sellInfoList:
+            infoData = sellInfo['data']
+            dealUid = sellInfo['uid']
+            title = sellInfo['title']  # .decode('unicode_escape')
+            data = (infoData[u'nickname'], infoData[u'update_time'], infoData[u'item_num'], dealUid, title,
+                    infoData[u'item_price'])
+            # print("ready write data", data)
+            # print(dealUid, "is ready write to sql")
 
-						for x in xrange(1,index):
-							str = str + "..."
-							pass
+            bHasData = False
+            try:
+                sql = "SELECT aliasId FROM aliasInfo WHERE aliasId = '%s'"
+                uidData = (dealUid)
+                self.__cursor.execute(sql % uidData)
+                bHasData = len(self.__cursor.fetchall()) > 0
+                pass
+            except Exception as e:
+                print("SELECT error", e.message)
+                pass
 
-						print('成功插入'+str)	
-						pass
-					except Exception as e:
-						print("INSERT error",e.message)
-						pass
-						print("sql not data ,we will write in ....")
-					pass
-				else:
-					print("data is has , we will updata...")
-					# sql = "UPDATE aliasInfo  set buyer = '%s'"
-					# self.__cursor.execute(sql % ("aaa"))
-					# self.__connect.commit()
-				pass
+            if bHasData is False:
+                try:
+                    # 插入数据
+                    sql = "INSERT INTO aliasInfo (buyer, time, buyCount, aliasId, title, price) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')"
+                    self.__cursor.execute(sql % data)
+                    self.__connect.commit()
 
-			print('录入结束')
-		pass
+                    index = index + 1
+                    if index > 8:
+                        index = 0
+                        pass
+                    str = ""
 
-	# 关闭连接
-	def closeLinkSql(self):
-		self.__cursor.close()
-		self.__connect.close()
-		pass
+                    for x in xrange(1, index):
+                        str = str + "..."
+                        pass
 
-		# indexData = dic[a]
-		# 	# dealUid = dic[a]
-		# 	# print("================================>", dic[a])
-		# 	# print(a, indexData)
-		# 	for b in indexData:
-		# 		# 详细信息
-		# 		infoData = b['data']
-		# 		dealUid  = b['uid']
-		# 		titleData = dicKey[a]
+                    print('成功插入' + str)
+                    pass
+                except Exception as e:
+                    print("INSERT error", e.message)
+                    pass
+                    print("sql not data ,we will write in ....")
+                pass
+            else:
+                print("data is has , we will updata...")
+            # sql = "UPDATE aliasInfo  set buyer = '%s'"
+            # self.__cursor.execute(sql % ("aaa"))
+            # self.__connect.commit()
+            pass
 
-		# 		#查找数据
-		# 		try:
-		# 			sql = ""
-		# 			pass
-		# 		except Exception as e:
-		# 			print(e.message)
-		# 			pass
+        print('录入结束')
+    pass
 
-		# 		# 插入数据
-		# 		try:
-		# 			sql = "INSERT INTO aliasInfo (buyer, time, buyCount, aliasId, title, price) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')"
-		# 			data = (infoData[u'nickname'], infoData[u'update_time'], infoData[u'item_num'], dealUid, (titleData[u'title']).decode('unicode_escape'), infoData[u'item_price'])
-		# 			self.__cursor.execute(sql % data)
-		# 			self.__connect.commit()
-		# 			pass
-		# 		except Exception as e:
-		# 			print(e.message)
-		# 			pass
-				
-		# 		index = index+1
-		# 		if index>8:
-		# 			index = 0
-		# 			pass
-		# 		str = ""
 
-		# 		for x in xrange(1,index):
-		# 			str = str + "..."
-		# 			pass
+# 关闭连接
+def closeLinkSql(self):
+    self.__cursor.close()
+    self.__connect.close()
+    pass
 
-		# 		print('成功插入'+str)
-		# 		pass
-		# 	pass
+    # indexData = dic[a]
+    # 	# dealUid = dic[a]
+    # 	# print("================================>", dic[a])
+    # 	# print(a, indexData)
+    # 	for b in indexData:
+    # 		# 详细信息
+    # 		infoData = b['data']
+    # 		dealUid  = b['uid']
+    # 		titleData = dicKey[a]
+
+    # 		#查找数据
+    # 		try:
+    # 			sql = ""
+    # 			pass
+    # 		except Exception as e:
+    # 			print(e.message)
+    # 			pass
+
+    # 		# 插入数据
+    # 		try:
+    # 			sql = "INSERT INTO aliasInfo (buyer, time, buyCount, aliasId, title, price) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')"
+    # 			data = (infoData[u'nickname'], infoData[u'update_time'], infoData[u'item_num'], dealUid, (titleData[u'title']).decode('unicode_escape'), infoData[u'item_price'])
+    # 			self.__cursor.execute(sql % data)
+    # 			self.__connect.commit()
+    # 			pass
+    # 		except Exception as e:
+    # 			print(e.message)
+    # 			pass
+
+    # 		index = index+1
+    # 		if index>8:
+    # 			index = 0
+    # 			pass
+    # 		str = ""
+
+    # 		for x in xrange(1,index):
+    # 			str = str + "..."
+    # 			pass
+
+    # 		print('成功插入'+str)
+    # 		pass
+    # 	pass
